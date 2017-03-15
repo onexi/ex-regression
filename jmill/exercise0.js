b6ab4e0a60776e804719cd0765341aad1524ada2
JMill <Jonathan@JMillville.com>
2017-03-15 10:59:28 -0400

    Add and use getRandomUnitVector()

--- jmill/exercise0.js
+++ jmill/exercise0.js
@@ -1,6 +1,12 @@
 var exercise = {};
 
 function getRandomNumber()  { return (Math.random()*20-10);}
+function getRandomUnitVector(){ 
+	var x = Math.random()*2 -1; //between -1 and +1
+	var y = Math.random()*2 - 1;
+	var len = Math.sqrt(x*x + y*y);
+	return {x:x/len, y:y/len};
+	} ;
 
 exercise.run =  function (){
     exercise.index = 0;
@@ -11,8 +17,13 @@ exercise.run =  function (){
 };
 
 var makePoint = function(){
-    var dx = exercise.x[exercise.index] + getRandomNumber();
-    var dy = exercise.y[exercise.index] + getRandomNumber();
+    var oldx = exercise.x[exercise.index] + getRandomNumber();
+    var oldy = exercise.y[exercise.index] + getRandomNumber();
+    var point = getRandomUnitVector();
+    var dx = oldx + 10*point.x;
+    var dy = oldy + 10*point.y;
+
+    drawLine(oldx,oldy,dx,dy);
 
     drawPoint(dx,dy,'red');
     exercise.x.push(dx); exercise.y.push(dy);
