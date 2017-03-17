b773e27a7d6c40854dad332be94bae35fbd6f4d4
maverickhntr <jethomas@mit.edu>
2017-03-17 10:49:13 -0400

    'added

---
+++ maverickhntr/exercise0.js
@@ -0,0 +1,38 @@
+var exercise = {};
+
+function getRandomNumber()  { return (Math.random()*20-10);}
+function getRandomUnitVector(){
+    var x = Math.random()*2 - 1;
+    var y = Math.random()*2 - 1;
+    var len = Math.sqrt(x*x + y*y);
+
+    return {x:x/len, y:y/len}
+}
+
+
+exercise.run =  function (){
+    exercise.index = 0;
+    exercise.x  = [400];
+    exercise.y  = [400];
+
+    setInterval(makePoint, 1000);
+};
+
+
+
+var makePoint = function(){
+    var oldx = exercise.x[exercise.index];
+    var oldy = exercise.y[exercise.index];
+    var point = getRandomUnitVector();
+    var dx = oldx + 50*point.x;
+    var dy = oldy + 50*point.y;
+
+    drawPoint(dx,dy,'red');
+    exercise.x.push(dx); exercise.y.push(dy);
+    exercise.index++;
+
+    drawLine(oldx,oldy,dx,dy);
+
+
+};
+
